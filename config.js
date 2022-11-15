8edf85530f39f71d6777f64babc36867c640519d
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-11-15 11:25:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ global.psaweria = 'https://saweria.co/gilzzbotz'
 
 /*============== NOMOR ==============*/
 global.nomorbot = '6282334703987'
-global.nomorown = '6285730794089','6282141728189'
+global.nomorown = '6285730794089','6282156581100'
 global.namebot = 'Gilzz MD'
 global.nameown = 'Gilzz Azaa'
 
@@ -53,6 +53,7 @@ global.nameown = 'Gilzz Azaa'
 /*============== STAFF ==============*/
 global.owner = [
   ['6285730794089', 'Gilzz XD', 'true']
+  ['6282156581100', 'Alif', 'true']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
